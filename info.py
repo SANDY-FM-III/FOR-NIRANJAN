277607683e3775295f010ba179d773b49b553408
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2023-05-27 18:07:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ MELCOW_VID = environ.get('MELCOW_VID',"https://telegra.ph/file/b04dd068fc8e2c223
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS','1960614875,1163754519').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS','1960614875 1163754519').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS','').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS','').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
